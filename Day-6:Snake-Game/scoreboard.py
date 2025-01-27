44efe01010188459e6a4d5dd08747f5241619c40
KrimsonJosh <jumoinzad@gmail.com>
2025-01-27 14:55:39 -0600

    feat: file writing in snake game

--- Day-6:Snake-Game/scoreboard.py
+++ Day-6:Snake-Game/scoreboard.py
@@ -10,7 +10,8 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.high_score = 0
+        with open('data.txt', 'r') as file:
+            self.high_score = int(file.read())
         self.goto(0, 270)
         self.hideturtle()
         self.update_scoreboard()
@@ -18,17 +19,16 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-    '''
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-    '''
+
     # Resave highscore with score
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            self.high_score = self.score()
+            with open("data.txt", mode = "w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
+
     def increase_score(self):
         self.score += 1
         self.clear()
