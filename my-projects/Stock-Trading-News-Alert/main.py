47128260b5893d98d5431be2a6acbd7ddfc3c470
KrimsonJosh <jumoinzad@gmail.com>
2025-02-05 18:31:19 -0600

    add newsAPI with 3 latest sources from TSLA

--- my-projects/Stock-Trading-News-Alert/main.py
+++ my-projects/Stock-Trading-News-Alert/main.py
@@ -2,23 +2,32 @@ import requests
 import os
 from dotenv import load_dotenv
 
+#importing api keys / stock names
+
 load_dotenv()
+
 alpha_API = os.getenv("API_KEY")
+news_API = os.getenv("NEWS_API_KEY")
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
 need_news = False
+
+#params and requesting from alphavantage API
 
 parameters = {
     "apikey": alpha_API,
     "function":  "TIME_SERIES_DAILY",
     "symbol": STOCK ,
 }
-
 url = f'https://www.alphavantage.co/query'
 r = requests.get(url, params= parameters)
 r.raise_for_status()
 data = r.json() 
 data = data['Time Series (Daily)']
+
+#get two most recent dates and their values
 
 sorted_dates = sorted(data.keys(), reverse = True)
 latestday, previousday = sorted_dates[:2]
@@ -34,6 +43,9 @@ fields = {
     "5. volume": "Volume"
 }
 
+#find the difference between each key and see
+#if that difference is greater than 5%
+greatestChange = 0
 for key, label in fields.items():
     (latest_val) = float(latestvalue[key])
     (prev_val) =  float(previousvalue[key])
@@ -42,10 +54,29 @@ for key, label in fields.items():
     percent_change = ((latest_val - prev_val) / prev_val) * 100
     
     if abs(percent_change) >= 5:
+        greatestChange = max(greatestChange, percent_change)
         need_news = True
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+#get news from that stock (this case TSLA) using news API
+if True:
+    url = "https://newsapi.org/v2/everything"
+    params = {
+        "apiKey": news_API,
+        "q": "Tesla OR TSLA",
+        "sortBy": "publishedAt",
+        "language": "en",
+    }
+    r = requests.get(url, params)
+    r.raise_for_status()
+
+    data = r.json()
+
+    #get 3 most recent sources
+    data = data['articles']
+    sorted_articles = sorted(data, key = lambda x: x['publishedAt'], reverse= True)
+    recent_sources = [article['source']['name'] for article in sorted_articles[:3]]
+    print(recent_sources)
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
