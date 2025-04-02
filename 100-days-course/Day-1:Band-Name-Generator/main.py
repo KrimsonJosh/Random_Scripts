b2592b1bbbbf6d21fa6ed10ff1658c191fc2717c
Joshua <165484810+KrimsonJosh@users.noreply.github.com>
2025-04-02 15:37:53 -0500

    Delete 100-days-course directory

--- 100-days-course/Day-1:Band-Name-Generator/main.py
+++
@@ -1,4 +0,0 @@
-print("This is the band name generator")
-city = input("Which city you from bruh\n")
-pet = input("Which is the name of your pet\n")
-print("Your band name is "  + city + " " + pet)
--- 100-days-course/Day-2:Tip-Calculator/main.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator!")
-totalbill = input("What was the total bill?\n")
-totalbill = int(totalbill[1:])
-tip = int(input("How much tip would you like to give? 10, 12, or 15 percent \n"))
-people = int(input("How many people would you like to split the bill with? enter numerical value \n"))
-print("Each person should pay: $" + str((totalbill + (totalbill * (tip/100))) / people))
-
